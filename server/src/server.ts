c861306ca23aa92e754b70c24a77baeba750a5e0
Shariar2006 <shariarnafis86@gmail.com>
2025-07-09 07:33:28 +0600

    add .env file and move all backend code to server folder

---
+++ server/src/server.ts
@@ -0,0 +1,72 @@
+/* eslint-disable no-console */
+import mongoose from 'mongoose'
+import { Server } from 'http'
+import app from './app'
+import { envVars } from './app/config/env'
+
+let server: Server
+
+const startServer = async () => {
+    try {
+        await mongoose.connect(envVars?.DB_URL)
+    console.log('connect DB')
+    app.listen(envVars.PORT, ()=>{
+        console.log(`serve is listening to port ${envVars.PORT}`)
+    })
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+startServer()
+
+process.on("SIGTERM", ()=>{
+    console.log('SIGTERM signal... server shutting down')
+
+    if(server){
+        server.close(()=>{
+            process.exit(1)
+        })
+    }
+
+    process.exit(1)
+})
+
+process.on("SIGINT", ()=>{
+    console.log('SIGTERM signal... server shutting down')
+
+    if(server){
+        server.close(()=>{
+            process.exit(1)
+        })
+    }
+
+    process.exit(1)
+})
+
+process.on("unhandledRejection", (err)=>{
+    console.log('unhandled rejection... server shutting down', err)
+
+    if(server){
+        server.close(()=>{
+            process.exit(1)
+        })
+    }
+
+    process.exit(1)
+})
+
+process.on("uncaughtException", (err)=>{
+    console.log('unhandled rejection... server shutting down', err)
+
+    if(server){
+        server.close(()=>{
+            process.exit(1)
+        })
+    }
+
+    process.exit(1)
+})
+
+// Promise.reject(new Error('unhandled rejection'))
+// throw new Error('uncaught exception')
