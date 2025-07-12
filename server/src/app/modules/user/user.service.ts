e6a7ecbf5acc0c112e322220fd96ea67d64fee34
Shariar2006 <shariarnafis86@gmail.com>
2025-07-12 10:47:24 +0600

    check user exist or not

--- server/src/app/modules/user/user.service.ts
+++ server/src/app/modules/user/user.service.ts
@@ -1,13 +1,24 @@
-import { IUser } from "./user.interface"
+import { StatusCodes } from "http-status-codes";
+import AppError from "../../errorHelpers/AppError";
+import { IAuthProvider, IUser } from "./user.interface"
 import { User } from "./user.model";
 
 const createUser = async (payload: Partial<IUser>) => {
 
-    const { name, email } = payload;
+    const { email, ...rest } = payload;
+
+    const isUserExist = await User.findOne({email})
+
+    if(isUserExist){
+        throw new AppError(StatusCodes.BAD_REQUEST, 'User already exist')
+    }
+
+    const authProvider: IAuthProvider = {provider: "credentials", providerId: email as string}
 
     const user = await User.create({
-        name,
-        email
+        email,
+        auths: authProvider,
+        ...rest
     })
 
     return user
